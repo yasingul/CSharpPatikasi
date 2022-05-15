889a6302594afb8e3c683c3c41cf401306e428a8
yasingul <yasngl46@gmail.com>
2022-05-15 19:13:08 +0300

    Array Dizisi notları

--- Program.cs
+++ Program.cs
@@ -6,11 +6,7 @@ namespace ConsoleProgramlama
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("İsminizi Girin");
-            string name = Console.ReadLine();
-            Console.WriteLine("Soyadınızı Girin");
-            string surname = Console.ReadLine();
-            Console.WriteLine("Merhaba, " + name + " " + surname);
+            
         }
     }
 }
